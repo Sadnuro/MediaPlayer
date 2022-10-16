937024e4fcc6b5dbaa9f2817f10374f56f733d8c
Sadith Nuñez <sadnr0213@gmail.com>
2022-10-16 18:02:24 -0500

    Added: AutoPause when visibilitychange event was

--- src/plugins/AutoPause.js
+++ src/plugins/AutoPause.js
@@ -6,24 +6,33 @@ class AutoPause {
      */
     constructor( params = { threshold: 0.25 } ) { 
         this.threshold = params.threshold;
+        // Asigna contexto de la clase ( Existe {player} ) para que tome los atributos como this.something
+        // De lo contrario tomarian el contexto del objeto/metodo que los invoke
         this.handleIntersection = this.handleIntersection.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
     }
 
     run(player) {
         this.player = player;
+
         const observer = new IntersectionObserver(
             this.handleIntersection,
             { threshold: this.threshold  }
         );
-
         observer.observe(this.player.media);
+
+        document.addEventListener('visibilitychange', this.handleVisibilityChange);
     }
     
     handleIntersection(entries){
         const entry = entries[0]; // Toma el unico objeto HTML observado que nos interesa
-        console.log(entry)
-
         const isVisible = entry.intersectionRatio >= this.threshold;
+        isVisible ? this.player.play() : this.player.pause();
+    };
+
+    handleVisibilityChange(){
+        console.log(document.visibilityState)
+        const isVisible = document.visibilityState === 'visible';
         isVisible ? this.player.play() : this.player.pause();
     };
 
