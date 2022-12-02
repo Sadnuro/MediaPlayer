ce881767a6b9da9a088d86cfb4bbd999f4626cad
Sadith Nuñez <sadnr0213@gmail.com>
2022-12-02 00:36:39 -0500

    added: AdsPlugin implementation

--- src/MediaPlayer.ts
+++ src/MediaPlayer.ts
@@ -8,12 +8,26 @@ interface ConfigMediaPlayer {
 class MediaPlayer {
     media: HTMLMediaElement;
     plugins: Array<any>;
+    container: HTMLElement;
 
     constructor(config: ConfigMediaPlayer) {
         this.media = config.media; // Will save the video
         this.plugins = config.plugins || [];
-
+        this.initPlayer();
         this.initPlugins();
+    }
+    private initPlayer() {
+        // Crea contenerdor y mueve el video adentro
+        this.container = document.createElement('div');
+        this.container.style.position = 'relative';
+        this.media.parentNode?.insertBefore(this.container, this.media);
+        this.container.appendChild(this.media);
+    }
+
+    private initPlugins() {
+        this.plugins.forEach(plugin => {
+            plugin.run(this);
+        });
     }
     // Methods
     play() {
@@ -33,11 +47,6 @@ class MediaPlayer {
     }
     toggleMute() {
         this.media.muted ? this.unmute() : this.mute();
-    }
-    private initPlugins() {
-        this.plugins.forEach(plugin => {
-            plugin.run(this);
-        });
     }
 }
 
--- src/plugins/Ads/AdsPlugin.ts
+++ src/plugins/Ads/AdsPlugin.ts
@@ -5,34 +5,51 @@ class AdsPlugin {
     private ads: Ads;
     private player: MediaPlayer;
     private media: HTMLMediaElement;
-    private currentAd: Ad;
+    private currentAd;
+    private adsContainer: HTMLElement;
 
-    constructor(){
+    constructor() {
         this.ads = Ads.getInstance(); // Use Singleton Pattern
+        this.adsContainer = document.createElement('div');
         // Allow to keep class instance context when the method will be invoke as addEventListener callback at run() method
         this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
     }
-    run(player: MediaPlayer){
+    run(player: MediaPlayer) {
         this.player = player;
+        this.player.container.appendChild(this.adsContainer);
         this.media = this.player.media;
-        this.media.addEventListener('timeupdate', this.handleTimeUpdate );
+        this.media.addEventListener('timeupdate', this.handleTimeUpdate);
 
     }
 
     private handleTimeUpdate() {
         const currentTime = Math.floor(this.media.currentTime);
-        if(currentTime%30 === 0){
+        if (currentTime % 30 === 0) {
             this.renderAd();
         }
     }
 
     private renderAd() {
-        if (this.currentAd){
+        if (this.currentAd) {
             return;
         }
         const ad: Ad = this.ads.getAd()!;
         this.currentAd = ad;
-        console.log(ad);
+        this.adsContainer.innerHTML = `
+            <div class="ads">
+                <a  class="ads__link" href="${this.currentAd.url}" target="_blank">
+                    <img class="ads__img" src="${this.currentAd.imageUrl}" />
+                    <div class="ads__info">
+                        <h5 class="ads__title">${this.currentAd.title}</h5>
+                        <p class="ads__body">${this.currentAd.body}</p>
+                    </div>
+                </a>
+            </div>`;
+        
+            setTimeout(()=>{
+                this.currentAd = null;
+                this.adsContainer.innerHTML = '';
+            }, 10000)
     }
 }
 
